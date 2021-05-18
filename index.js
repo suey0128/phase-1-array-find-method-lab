c5f2144af15deb4cfb3c1c93b003cb3591c24b04
suey0128 <syui0128@gmail.com>
2021-05-18 15:43:13 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+
+// function superbowlWin(array) {
+//     return array['result'] === 'W';
+// }
+
+// record.find(superbowlWin).year;
+
+// record.find(superbowlWin);
+
+// function xxx(array) {
+//     return array['result'] === 'W';
+// }
+
+// array.find(xxx).year;
+
+
+function xxx(element) {
+    // console.log("my xxx array:" , array)
+    return element['result'] === 'W';
+}
+
+function superbowlWin(array) {
+    let result = array.find(xxx);
+    if (result) {
+        return result.year;
+    } else {
+        return result;
+    }
+}
